49c8c850e6bbf53a4f307a35891e0848e2a30772
Joseph Sutker <joseph.sutker@students.olin.edu>
2017-04-27 15:34:42 -0400

    sets up fsm better

--- neato_commands/src/fsm.cpp
+++ neato_commands/src/fsm.cpp
@@ -1,23 +1,32 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <sstream>
+#include "geometry_msgs/Twist.h"
 
-void command_callback(const std_msgs::String::ConstPtr msg) {
-  switch(msg->data.c_str()) {
-    case "follow":
-      break;
-    case "stop":
-      break;
-    case "dance":
-      break;
-  }
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+std_msgs::String command;
+
+void commandCallback(const std_msgs::String::ConstPtr msg) {
+  command = msg->data.c_str();
 }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "talker");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe<std_msgs::String>("actual_command", 1000, command_callback);
-  ros::spin();
+  ros::Subscriber sub = n.subscribe<std_msgs::String>("actualCommand", 1000, command_callback);
+  ros::Publisher movement_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000)
+  ros::Rate loop_rate(1);
+
+  while(ros::ok()) {
+    ros::spinOnce();
+    switch(command) {
+      case "follow":
+        break;
+      case "stop":
+        break;
+      case "dance":
+        break;
+    }
+    loop_rate.sleep();
+  }
+
   return 0;
 }

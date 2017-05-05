842ba6757e125b761389111a7b788002287a34a1
Joseph Sutker <joseph.sutker@students.olin.edu>
2017-05-05 02:22:10 -0400

    corrects callback name

--- neato_commands/src/fsm.cpp
+++ neato_commands/src/fsm.cpp
@@ -17,7 +17,7 @@ void commandCallback(const std_msgs::String::ConstPtr msg) {
 int main(int argc, char **argv) {
   ros::init(argc, argv, "talker");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe<std_msgs::String>("actualCommand", 1000, command_callback);
+  ros::Subscriber sub = n.subscribe<std_msgs::String>("interpreted_command", 1000, commandCallback);
   ros::Publisher movement_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000)
   ros::Rate loop_rate(1);
 
@@ -28,17 +28,14 @@ int main(int argc, char **argv) {
       case "go":
         setTwistVals(twist, 1, 0);
         break;
-      case "back":
-        setTwistVals(twist, -0.5, 0);
+      case "stop":
+        setTwistVals(twist, 0, 0);
         break;
       case "left":
         setTwistVals(twist, 0, 0.5);
         break;
       case "right":
         setTwistVals(twist, 0, -0.5);
-        break;
-      case "stop":
-        setTwistVals(twist, 0, 0);
         break;
       case "dance":
         double rand_x = 2*double(rand())/double(RAND_MAX)-1;

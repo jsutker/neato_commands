459d51d3987531e7f15b8930417b024a81780d68
Joseph Sutker <joseph.sutker@students.olin.edu>
2017-05-05 02:35:07 -0400

    adds comments on what to do, sets up audio_input variable

--- neato_commands/src/stt.cpp
+++ neato_commands/src/stt.cpp
@@ -3,9 +3,10 @@
 #include <stdlib.h>
 
 std_msgs::String command;
+std_msgs::String audio_input;
 
 void audioCallback(const std_msgs::String::ConstPtr msg) {
-  command = msg->data.c_str();
+  audio_input = msg->data.c_str();
 }
 
 int main(int argc, char **argv) {
@@ -15,7 +16,12 @@ int main(int argc, char **argv) {
   ros::Publisher pub = n.advertise<std_msgs::String>("interpreted_command", 1000)
   ros::Rate loop_rate(1);
 
+  // set up ML algorithm
+
+  // train ML algorithm
+
   while(ros::ok()) {
+    // translate audio_input to command using ML algorithm implemented above
     
     pub.publish(command);
     ros::spinOnce();
